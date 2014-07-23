0ff469b60d2d4b7377497c9a4f55ed9463b47008
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-23 20:27:01 +0700

    Adjust to Getopt::Long::Util 0.77

--- lib/Perinci/Sub/GetArgs/Argv.pm
+++ lib/Perinci/Sub/GetArgs/Argv.pm
@@ -215,8 +215,8 @@ sub gen_getopt_long_spec_from_meta {
         my $handler = $v->{handler};
         my $res = parse_getopt_long_opt_spec($ospec)
             or return [400, "Can't parse common opt spec '$ospec'"];
-        $go_spec{ $res->{normalized} } = $handler;
-        $specmeta{ $res->{normalized} } = {arg=>undef, orig_spec=>$ospec, parsed=>$res};
+        $go_spec{$ospec} = $handler;
+        $specmeta{$ospec} = {arg=>undef, orig_spec=>$ospec, parsed=>$res};
         for (@{ $res->{opts} }) {
             return [412, "Clash of common opt '$_'"] if $seen_opts{$_};
             $seen_opts{$_}++; $seen_common_opts{$_} = $ospec;
